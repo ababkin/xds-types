35b1593701fa4ce6493a604d0070bdf40098c331
Alex Babkin <ababkin@sumall.com>
2015-10-01 13:44:02 -0400

    add getDownloadedParams helper

--- src/Xds/Directive/Util.hs
+++ src/Xds/Directive/Util.hs
@@ -4,8 +4,17 @@
 module Xds.Directive.Util where
 
 import Xds.Types (URL)
-import Xds.Directive.Types (Directive(Directive, results), 
+import Xds.Directive.Types (Directive(Directive, params, results), 
+  Param(DownloadParams),
   Result(DownloadSuccess, downloadedUrl))
+
+getDownloadedParams :: Directive -> Maybe Param
+getDownloadedParams dir@Directive{ params = [] } = 
+  Nothing
+getDownloadedParams Directive{ params = (dp@DownloadParams{}):_ } = 
+  Just dp 
+getDownloadedParams dir@Directive{ params = _:ps } = 
+  getDownloadedParams dir{ params = ps }
 
 
 getDownloadedUrl :: Directive -> Maybe URL
