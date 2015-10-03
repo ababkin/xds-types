d7ecbac4b9817e22b4509766a539a2f9abaafed0
Alex Babkin <ababkin@sumall.com>
2015-10-03 17:52:37 -0400

    wip

--- src/Xds/Directive/Types.hs
+++ src/Xds/Directive/Types.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module Xds.Directive.Types where
 
 import Data.Text (Text)
+import qualified Data.Text as T (unpack)
 import Data.Map (Map)
 import GHC.Generics (Generic)
 import Data.Vector (Vector)
@@ -29,17 +32,62 @@ data Param = DownloadParams {
   , s3Bucket  :: URL
   , s3Path    :: Path
   } deriving (Show, Generic)
-instance FromJSON Param
-instance ToJSON Param
+instance FromJSON Param where
+  parseJSON o@(Object v) = do
+    (t :: Text) <- v .: "type"
+    case t of
+      "download" -> DownloadParams
+        <$> v .: "remoteUrl"
+        <*> v .: "s3Bucket"
+        <*> v .: "s3Path"
+      _ -> 
+        typeMismatch ("unexpected params type: " ++ T.unpack t) o
+instance ToJSON Param where
+  toJSON (DownloadParams {..})  =
+    object  [
+              "type"      .= ("download" :: Text)
+            , "remoteUrl" .= remoteUrl
+            , "s3Bucket"  .= s3Bucket
+            , "s3Path"    .= s3Path
+            ]
+  toJSON params = error $ "could not serialize params to json: " ++ show params
 
 data Result = DownloadSuccess {
     downloadedUrl :: URL
-  } | DownloadFail Error 
+  } | DownloadFailure Error 
     | ProcessSuccess {
     stats :: Stats 
-  } | ProcessFail Error
+  } | ProcessFailure Error
   deriving (Show, Generic)
-instance FromJSON Result
-instance ToJSON Result
+instance FromJSON Result where
+  parseJSON o@(Object v) = do
+    (t :: Text) <- v .: "type"
+    (s :: Text) <- v .: "status"
+    case (t,s) of
+      ("download", "success") -> DownloadSuccess
+        <$> v .: "downloadedUrl"
+      ("download", "failure") -> DownloadFailure
+        <$> v .: "error"
+      ("process", "success") -> ProcessSuccess
+        <$> v .: "stats"
+      ("process", "failure") -> ProcessFailure
+        <$> v .: "error"
+      _ -> 
+        typeMismatch ("unexpected type: " ++ T.unpack t ++ 
+                      " and status: " ++ T.unpack s) o
+instance ToJSON Result where
+  toJSON (DownloadSuccess {..})  =
+    object  [
+              "type"          .= ("download" :: Text)
+            , "status"        .= ("success" :: Text)
+            , "downloadedUrl" .= downloadedUrl
+            ]
+  toJSON (DownloadFailure err)  =
+    object  [
+              "type"          .= ("download" :: Text)
+            , "status"        .= ("failure" :: Text)
+            , "error"         .= err 
+            ]
 
+  toJSON result = error $ "could not serialize result to json: " ++ show result 
 
